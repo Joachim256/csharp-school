cb0b97e47248566468289112d890e42fb1a85c9c
Joachim256 <jachymnet@seznam.cz>
2022-11-11 11:57:29 +0100

    Zadání čísel

--- csharp-school/Program.cs
+++ csharp-school/Program.cs
@@ -1,5 +1,20 @@
 ﻿internal class Program{
 	private static void Main(string[] args){
-		Console.WriteLine("Hello world!");
+		int prvniCislo = 0;
+		int druheCislo = 0;
+		int tretiCislo = 0;
+
+		try{
+			Console.Write("Zadejte první číslo:");
+			prvniCislo = int.Parse(Console.ReadLine());
+			Console.Write("Zadejte druhé číslo:");
+			druheCislo = int.Parse(Console.ReadLine());
+			Console.Write("Zadejte třetí číslo:");
+			tretiCislo = int.Parse(Console.ReadLine());
+		}catch(Exeption e){
+			Console.WriteLine("Toto není číslo!");
+			return;
+		}
+
 	}
 }
