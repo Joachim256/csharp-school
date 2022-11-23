355873ab50394dd6d9bdeb72fffb1360aa8ada81
Joachim256 <jachymnet@seznam.cz>
2022-11-23 13:14:54 +0100

    Nested for loop
    use of ternary (conditional) operator (c ? true : false)

--- csharp-school/Program.cs
+++ csharp-school/Program.cs
@@ -1,5 +1,11 @@
 ﻿internal class Program{
 	private static void Main(string[] args){
-		Console.WriteLine("Hello world!");
+		for(int y = 1; y <= 10; y++){
+			for(int x = 1; x <= 10; x++){
+				int n = x * y;
+				Console.Write((n < 10 ? " " : "") + n + (x == 10 ? "" : " "));
+			}
+			Console.WriteLine();
+		}
 	}
 }
