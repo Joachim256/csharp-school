61ea18246ec6c4f8c15ec498396fe5711e496462
joachim256 <jachymnet@seznam.cz>
2022-11-23 09:33:08 +0100

    Input numbers

--- csharp-school/Program.cs
+++ csharp-school/Program.cs
@@ -1,5 +1,35 @@
 ﻿internal class Program{
 	private static void Main(string[] args){
-		Console.WriteLine("Hello world!");
+		int zaklad, exp;
+		int sum = 1;
+
+        Console.WriteLine("Tento program po zadání základu mocniny a exponentu vypíše výsledek (a^n).");
+
+        while (true) {
+            try {
+                Console.Write("Zadejte základ mocniny: ");
+                zaklad = int.Parse(Console.ReadLine());
+            } catch (Exception e) {
+                Console.WriteLine("Toto není číslo!"); continue;
+            }
+            break;
+        }
+        while(true){
+            try{
+                Console.Write("Zadejte exponent: ");
+                exp = int.Parse(Console.ReadLine());
+            }catch (Exception e){
+                Console.WriteLine("Toto není číslo!"); continue;
+            }
+            if (exp < 0){
+                Console.WriteLine("Zadejte prosím kladný exponent"); continue;
+            }
+            break;
+        }
+
+		for(int i = 0; i < exp; i++){
+			sum *= zaklad;
+		}
+		Console.WriteLine(sum);
 	}
 }
