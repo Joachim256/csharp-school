873a7a9ab51e3f6001199e7ef85c3a04d4cffac7
joachim256 <jachymnet@seznam.cz>
2022-12-07 09:01:52 +0100

    Kalkulačka 1.0

--- csharp-school/Program.cs
+++ csharp-school/Program.cs
@@ -3,12 +3,52 @@
 		Console.WriteLine("Kalkulačka 1.0");
 
 		int c1, c2;
+		string op;
 
-		Console.Write("Zadejte první číslo: ");
-		try{
-			c1 = int.Parse(Console.ReadLine());
-		}catch(Exception e){
-			
+		while (true){
+			Console.Write("Zadejte první číslo: ");
+			try{
+				c1 = int.Parse(Console.ReadLine());
+			}
+			catch (Exception e){
+				Console.WriteLine("Toto není číslo!");
+				continue;
+			}
+			break;
 		}
+		while (true){
+			Console.Write("Zadejte operaci (+ - * /): ");
+			op = Console.ReadLine();
+
+			if(op != "+" && op != "-" && op != "*" && op != "/"){
+				Console.WriteLine("Neplatná operace!");
+				continue;
+			}
+			break;
+		}
+		while (true){
+			Console.Write("Zadejte druhé číslo: ");
+			try{
+				c2 = int.Parse(Console.ReadLine());
+			}
+			catch (Exception e){
+				Console.WriteLine("Toto není číslo!");
+				continue;
+			}
+			break;
+		}
+
+		float vysledek = 0;
+		if(op == "+"){
+			vysledek = c1 + c2;
+		}else if(op == "-"){
+			vysledek = c1 - c2;
+		}else if(op == "*"){
+			vysledek = c1 * c2;
+		}else if(op == "/"){
+			vysledek = (float)c1 / (float)c2;
+		}
+
+		Console.WriteLine("Výsledek je {0}", vysledek);
 	}
 }
