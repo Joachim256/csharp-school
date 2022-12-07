d1c798077ca065cb299b16fccbec14fc93147320
joachim256 <jachymnet@seznam.cz>
2022-12-07 08:28:30 +0100

    Switch case

--- csharp-school/Program.cs
+++ csharp-school/Program.cs
@@ -1,5 +1,34 @@
 ﻿internal class Program{
 	private static void Main(string[] args){
-		Console.WriteLine("Hello world!");
+		Console.Write("Zadejte smajlík: ");
+		String? smajlik = Console.ReadLine();
+
+		if (smajlik == null) {
+			Console.WriteLine("Neznámá chyba");
+		}
+
+		switch(smajlik){
+			case ":-)":
+				Console.WriteLine("Smajlík je veselý");
+				break;
+            case ":)":
+                Console.WriteLine("Smajlík je veselý");
+                break;
+            case ":-(":
+				Console.WriteLine("Smajlík je smutný");
+				break;
+            case ":(":
+                Console.WriteLine("Smajlík je smutný");
+                break;
+            case ":-*":
+				Console.WriteLine("Smajlík je zamilovaný");
+				break;
+			case ":-P":
+				Console.WriteLine("Smajlík má vyplazený jazyk");
+				break;
+			default:
+				Console.WriteLine("Neznámý smajlík");
+				break;
+		}
 	}
 }
