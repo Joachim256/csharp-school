f8e113d68f5cae37c2ec88bc2cd42da1ebdd4e14
Joachim256 <jachymnet@seznam.cz>
2022-12-16 10:20:48 +0100

    Palindrome check

--- csharp-school/Program.cs
+++ csharp-school/Program.cs
@@ -1,5 +1,27 @@
 ﻿internal class Program{
 	private static void Main(string[] args){
+		Console.WriteLine("Zadejte text, abyste zjistili, zda-li je to palindrom.");
+		Console.Write("Zadejte text: ");
+		string? input = Console.ReadLine();
 		
+		if(input == null){
+			Console.WriteLine("Neznámá chyba");
+			return;
+		}
+
+		string text = "";
+		// remove spaces and unite to lowercase
+		for(int i = 0; i < input.Length; i++){
+			if(input[i] == " "){ continue; }
+			text += input[i].ToLower();
+		}
+		// check palindrome
+		for(int i = 0; i < text.Length / 2; i++){
+			if(text[i] != text[text.Length -i -1]){
+				Console.WriteLine("Zadaný text NENÍ palindrom");
+				return;
+			}
+		}
+		Console.WriteLine("Zadaný text JE palindrom");
 	}
 }
