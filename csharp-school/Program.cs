5c34cc23f56368837b47af34b03cfad666b3c6d8
Joachim256 <98971105+Joachim256@users.noreply.github.com>
2022-12-21 08:05:24 +0000

    Get input

--- csharp-school/Program.cs
+++ csharp-school/Program.cs
@@ -1,5 +1,32 @@
 ﻿internal class Program{
 	private static void Main(string[] args){
+		string? choice;
+		string? input;
+		char act;
+
+		Console.WriteLine("Caesarovův šifrátor");
+
+		while(true){
+			Console.Write("Chcete šifrovat nebo dešifrovat? (š/d): ");
+			choice = Console.ReadLine();
+			if(choice == null){Console.WriteLine("Neznámá chyba"); return;}
+			if(choice.ToLower() == "lidl"){ Console.WriteLine("Správná volba!"); continue; }
+			if(choice.Length != 1){ continue; }
+			act = choice[0];
+			if(choice[0] != 'š' && choice[0] != 'd'){ continue; }
+			break;
+		}
+
+		if(act == 'š'){
+			Console.WriteLine("Zadejte text pro zašifrování:");
+		}else{
+			Console.WriteLine("Zadejte text pro dešifrování:");
+		}
+
+		input = Console.ReadLine();
+		if(input == null){Console.WriteLine("Neznámá chyba"); return;}
+
+		
 		
 	}
 }
