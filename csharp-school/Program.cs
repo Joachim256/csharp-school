d9f6932c9c8d877974ff3138105392fd6bcf261f
Joachim256 <98971105+Joachim256@users.noreply.github.com>
2023-01-04 07:25:16 +0000

    Euklidův odčítací algoritmus

--- csharp-school/Program.cs
+++ csharp-school/Program.cs
@@ -2,4 +2,15 @@
 	private static void Main(string[] args){
 		
 	}
+	// Euklidův odčítací algoritmus
+	private static int SubEuclid(int u, int w){
+		while(u != w){
+			if(u > w){
+				u = u - w;
+			}else{
+				w = w - u;
+			}
+		}
+		return u;
+	}
 }
