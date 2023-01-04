a69c03e7a393fb019ceb890fd65ee5e1115ff27d
Joachim256 <98971105+Joachim256@users.noreply.github.com>
2023-01-04 07:37:38 +0000

    User interface

--- csharp-school/Program.cs
+++ csharp-school/Program.cs
@@ -1,6 +1,28 @@
 ﻿internal class Program{
 	private static void Main(string[] args){
-		
+		int a, b;
+		Console.WriteLine("Implementace Euklidova odečítacího a dělícího algoritmu pro nalezení největšího společného dělitele");
+
+		while(true){
+			Console.Write("Zadejte první číslo: ");
+			try{
+				a = int.Parse(Console.ReadLine());
+			}catch(Exception e){
+				Console.WriteLine("Toto není číslo!"); continue;
+			}
+			break;
+		}
+		while(true){
+			Console.Write("Zadejte druhé číslo: ");
+			try{
+				b = int.Parse(Console.ReadLine());
+			}catch(Exception e){
+				Console.WriteLine("Toto není číslo!"); continue;
+			}
+			break;
+		}
+
+		Console.WriteLine("Největší společný dělitel zadaných čísel spočítaný:\nEuklidovým odčítacím algoritmem: {0}\nEuklidovým dělícím algoritmem: {1}", SubEuclid(a,b), DivEuclid(a,b));
 	}
 	// Euklidův odčítací algoritmus
 	private static int SubEuclid(int u, int w){
