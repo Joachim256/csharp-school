6acfc7ab6c3e888c0be9994f9d45bbc2b601fa68
joachim256 <jachymnet@seznam.cz>
2023-01-18 09:43:12 +0100

    Osobní údaje

--- WinFormsApp2/Form1.Designer.cs
+++ WinFormsApp2/Form1.Designer.cs
@@ -28,12 +28,259 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonFemale = new System.Windows.Forms.RadioButton();
+            this.radioButtonMale = new System.Windows.Forms.RadioButton();
+            this.surname = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.birthDate = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lifeState = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.today = new System.Windows.Forms.DateTimePicker();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.surname);
+            this.panel1.Controls.Add(this.name);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(288, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonFemale);
+            this.groupBox1.Controls.Add(this.radioButtonMale);
+            this.groupBox1.Location = new System.Drawing.Point(176, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(93, 65);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pohlaví";
+            // 
+            // radioButtonFemale
+            // 
+            this.radioButtonFemale.AutoSize = true;
+            this.radioButtonFemale.Checked = true;
+            this.radioButtonFemale.Location = new System.Drawing.Point(3, 40);
+            this.radioButtonFemale.Name = "radioButtonFemale";
+            this.radioButtonFemale.Size = new System.Drawing.Size(49, 19);
+            this.radioButtonFemale.TabIndex = 1;
+            this.radioButtonFemale.TabStop = true;
+            this.radioButtonFemale.Text = "žena";
+            this.radioButtonFemale.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonMale
+            // 
+            this.radioButtonMale.AutoSize = true;
+            this.radioButtonMale.Location = new System.Drawing.Point(3, 19);
+            this.radioButtonMale.Name = "radioButtonMale";
+            this.radioButtonMale.Size = new System.Drawing.Size(48, 19);
+            this.radioButtonMale.TabIndex = 0;
+            this.radioButtonMale.Text = "muž";
+            this.radioButtonMale.UseVisualStyleBackColor = true;
+            // 
+            // surname
+            // 
+            this.surname.Location = new System.Drawing.Point(70, 51);
+            this.surname.Name = "surname";
+            this.surname.Size = new System.Drawing.Size(100, 23);
+            this.surname.TabIndex = 3;
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(70, 16);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(100, 23);
+            this.name.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(3, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Příjmení:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Jméno:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.birthDate);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.lifeState);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(12, 118);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(288, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // birthDate
+            // 
+            this.birthDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.birthDate.Location = new System.Drawing.Point(142, 48);
+            this.birthDate.Name = "birthDate";
+            this.birthDate.Size = new System.Drawing.Size(127, 23);
+            this.birthDate.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(26, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 19);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Datum narození:";
+            // 
+            // lifeState
+            // 
+            this.lifeState.FormattingEnabled = true;
+            this.lifeState.Items.AddRange(new object[] {
+            "svobodný/á",
+            "ženatý/vdaná",
+            "rozvedený/á",
+            "vdovec/vdova"});
+            this.lifeState.Location = new System.Drawing.Point(70, 22);
+            this.lifeState.Name = "lifeState";
+            this.lifeState.Size = new System.Drawing.Size(100, 23);
+            this.lifeState.TabIndex = 1;
+            this.lifeState.Text = "ženatý/vdaná";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(26, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Stav:";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.today);
+            this.panel3.Location = new System.Drawing.Point(12, 224);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(288, 100);
+            this.panel3.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(12, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Dnes:";
+            // 
+            // today
+            // 
+            this.today.Enabled = false;
+            this.today.Location = new System.Drawing.Point(61, 40);
+            this.today.Name = "today";
+            this.today.Size = new System.Drawing.Size(200, 23);
+            this.today.TabIndex = 0;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(306, 12);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Přidat";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(387, 10);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(356, 314);
+            this.output.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(755, 339);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Button buttonAdd;
+        private TextBox output;
+        private Label label2;
+        private Label label1;
+        private TextBox surname;
+        private TextBox name;
+        private GroupBox groupBox1;
+        private RadioButton radioButtonFemale;
+        private RadioButton radioButtonMale;
+        private Label label3;
+        private ComboBox lifeState;
+        private Label label4;
+        private DateTimePicker birthDate;
+        private DateTimePicker today;
+        private Label label5;
     }
 }
--- WinFormsApp2/Form1.cs
+++ WinFormsApp2/Form1.cs
@@ -6,5 +6,33 @@ namespace WinFormsApp2
         {
             InitializeComponent();
         }
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string addText;
+            bool gender;
+            int genderint;
+            if (radioButtonMale.Checked)
+            {
+                gender = false;
+                genderint = 0;
+            }
+            else
+            {
+                gender = true;
+                genderint = 1;
+            }
+            Dictionary<bool, string> genderMap = new Dictionary<bool, string> { { false, "muž" }, { true, "žena" } };
+            string[] x1 = { "svobodný", "svobodná" }; string[] x2 = { "ženatý", "vdaná" }; string[] x3 = { "rozvedený", "rozvedená"}; string[] x4 = { "vdovec", "vdova" };
+            Dictionary<string, string[]> lifeStateMap = new Dictionary<string, string[]> {
+                { "svobodný/á", x1 },
+                { "ženatý/vdaná", x2 },
+                { "rozvedený/á", x3 },
+                { "vdovec/vdova", x4 }
+            };
+            addText = "\r\n" + name.Text + " " + surname.Text + ", " + genderMap[gender] + ", " + lifeStateMap[lifeState.Text][genderint] + ", "
+                + " narozen" + (gender ? "a " : " ") + birthDate.Text + ", "
+                + "datum zápisu: " + today.Text;
+            output.AppendText(addText);
+        }
     }
 }
