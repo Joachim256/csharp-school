f54cfc92e43b41763b8acc10cc9434505085ce8a
joachim256 <jachymnet@seznam.cz>
2023-02-15 09:09:15 +0100

    Průměr známek

--- csharp-school/Program.cs
+++ csharp-school/Program.cs
@@ -1,5 +1,36 @@
 ﻿internal class Program{
 	private static void Main(string[] args){
-		
+		Console.WriteLine("Aplikace Průměr známek v1.0");
+		int pocet = 0;
+		while (pocet == 0)
+		{
+			try
+			{
+                Console.Write("Zadejte počet známek: ");
+                pocet = int.Parse(Console.ReadLine());
+				if (pocet == 0) { Console.WriteLine("Prosím, alespoň jednu známku ;)"); }
+			}
+			catch(Exception e){}
+		}
+		uint[] znamky = new uint[pocet];
+		for(int i = 0; i < pocet; i++)
+		{
+            while (true)
+            {
+                Console.Write("\nZadejte známku (č. {0}): ", i + 1);
+				char z = Console.ReadKey().KeyChar;
+				znamky[i] = (uint)(z - 0x30);
+				if(z >= 0x30 && z <= 0x39) { break; }
+            }
+        }
+		Console.WriteLine();
+		Console.WriteLine("Zadali jste známky: {0}", String.Join("; ", znamky));
+		uint suma = 0;
+		for (int i = 0; i < pocet; i++)
+		{
+			suma += znamky[i];
+		}
+		float avg = (float)suma / pocet;
+		Console.WriteLine("Váš průměr je: {0}", avg);
 	}
 }
