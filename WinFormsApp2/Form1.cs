709a19ac963599084ffd6d93e9e74d637ec7ab03
joachim256 <jachymnet@seznam.cz>
2023-03-22 09:06:51 +0100

    Generate numbers

--- WinFormsApp2/Form1.cs
+++ WinFormsApp2/Form1.cs
@@ -2,6 +2,10 @@ namespace WinFormsApp2
 {
     public partial class Form1 : Form
     {
+        const int gridSize = 10;
+        const int cellSize = 30;
+
+        int[,] data = new int[gridSize, gridSize];
         public Form1()
         {
             InitializeComponent();
@@ -9,8 +13,33 @@ namespace WinFormsApp2
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            grid.ColumnCount = 10;
-            grid.RowCount = 10;
+            InitTable();
+            GenerateNumbers();
+        }
+        private void GenerateNumbers()
+        {
+            Random rng = new Random();
+            for(int x = 0; x < gridSize; x++)
+            {
+                for(int y = 0; y < gridSize; y++)
+                {
+                    // generate random number
+                    int n = rng.Next(0, 100);
+                    data[x, y] = n;
+                    // render
+                    grid.Rows[y].Cells[x].Value = n;
+                }
+            }
+        }
+        private void InitTable()
+        {
+            grid.ColumnCount = gridSize;
+            grid.RowCount = gridSize;
+            for (int i = 0; i < gridSize; i++)
+            {
+                grid.Columns[i].Width = cellSize;
+                grid.Rows[i].Height = cellSize;
+            }
         }
     }
 }
