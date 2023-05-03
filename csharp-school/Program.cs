9c094848678607202b751f99557da37407bfaf8a
joachim256 <jachymnet@seznam.cz>
2023-05-03 09:11:53 +0200

    ExtractNumbers function

--- csharp-school/Program.cs
+++ csharp-school/Program.cs
@@ -1,5 +1,31 @@
 ﻿internal class Program{
 	private static void Main(string[] args){
-		
+		Console.WriteLine("Textová sčítačka v0.1");
+	}
+	private static List<int> ExtractNumbers(string input)
+	{
+		var numbers = new List<int>();
+		string num = "";
+
+		foreach(char a in input)
+		{
+			if(a >= 0x30 && a <= 0x39)
+			{
+				// it's a number character
+				num += a;
+			}
+			else
+			{
+				if(num.Length == 0){ continue; }
+				numbers.Add(int.Parse(num));
+				num = "";
+			}
+		}
+		if(num.Length > 0)
+		{
+            numbers.Add(int.Parse(num));
+        }
+
+		return numbers;
 	}
 }
