0b8beab5ec574905e1b28fc7f9ba658055b0a87b
joachim256 <jachymnet@seznam.cz>
2023-05-10 09:36:23 +0200

    Add count & print

--- csharp-school/Program.cs
+++ csharp-school/Program.cs
@@ -1,16 +1,22 @@
 ﻿internal class Program{
 	private static void Main(string[] args){
-		Console.WriteLine("Jmenný seznam v0.1");
+		Console.WriteLine("Jmenný seznam v1.0");
 
 		Console.WriteLine("Zadávejte jména, která se budou řadit do abecedního seznamu. Pro ukončení zadejte @ a seznam bude vypsán.");
 
 		string[] names = new string[26];
+		int[] counts = new int[26];
 
 		while (true)
 		{
 			string name = Console.ReadLine();
 			if(name == "@" || name == "")
 			{
+				for(int i = 0; i < 26; i++)
+				{
+					if (counts[i] == 0) { continue; }
+					Console.WriteLine("({0}) {1}", counts[i], names[i]);
+				}
 				break;
 			}
 			if (name[0] >= 97 && name[0] <= 122)
@@ -25,8 +31,9 @@
 			}
 
 			int index = name[0] - 65;
-			if (names[index] != "") { names[index] += ", "; }
+			if (counts[index] != 0) { names[index] += ", "; }
 			names[index] += name;
+			counts[index]++;
 		}
 	}
 }
