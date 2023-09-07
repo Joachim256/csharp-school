fbab8a8bf5d01e050a0404a329388ab7c6df810f
joachim256 <jachymnet@seznam.cz>
2023-09-07 08:31:36 +0200

    Input number

--- csharp-school/Program.cs
+++ csharp-school/Program.cs
@@ -1,5 +1,28 @@
 ﻿internal class Program{
 	private static void Main(string[] args){
-		
+		Console.WriteLine("Stromeček v0.1");
+		Console.WriteLine("Zadejte liché číslo větší než 3 a bude Vám vykreslen stromeček o zadané šířce:");
+
+		int n;
+		while (true)
+		{
+            string N = Console.ReadLine();
+			try
+			{
+				n = int.Parse(N);
+				if(n < 4 || n % 2 != 1)
+				{
+					Console.WriteLine("Zadejte prosím liché číslo větší než 3");
+					continue;
+				}
+			} catch (Exception ex)
+			{
+				Console.WriteLine("Zadejte prosím číslo");
+				continue;
+			}
+			break;
+        }
+
+		Console.WriteLine("číslo je {0}", n);
 	}
 }
