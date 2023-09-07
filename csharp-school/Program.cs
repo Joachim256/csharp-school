5203244a81e92601ea92ced877c55bca3c10b29e
joachim256 <jachymnet@seznam.cz>
2023-09-07 09:03:41 +0200

    Run through and print

--- csharp-school/Program.cs
+++ csharp-school/Program.cs
@@ -22,7 +22,28 @@
 			}
 			break;
 		}
+        Console.Write("Rozklad: {0} = ", n);
 
-		Console.WriteLine("Zadané číslo je {0}", n);
+        List<int> cisla = new List<int>();
+
+		for(int i = 2; ; i++)
+		{
+			while(n % i == 0)
+			{
+				n = n / i;
+				cisla.Add(i);
+			}
+			if(n == 1)
+			{
+				break;
+			}
+		}
+
+		for(int i = 0; i < cisla.Count; i++)
+		{
+			Console.Write("{0}", cisla[i]);
+			if(i != cisla.Count - 1) { Console.Write(" * "); }
+		}
+		Console.WriteLine();
 	}
 }
