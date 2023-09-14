dd2d73c2a2c4d0b2f8c6e8c228cf72071c8ac6fb
joachim256 <jachymnet@seznam.cz>
2023-09-14 09:18:44 +0200

    Pass by reference

--- csharp-school/Program.cs
+++ csharp-school/Program.cs
@@ -1,5 +1,32 @@
 ﻿internal class Program{
 	private static void Main(string[] args){
-		
+		Console.Write("Zadejte základní cenu: ");
+		string zaklad = Console.ReadLine();
+		Console.Write("Zadejte zdražení v procentech: ");
+		string zdrazeni = Console.ReadLine();
+		zdrazeni = zdrazeni.Replace("%", "");
+
+		double cena, procent;
+		try
+		{
+			cena = double.Parse(zaklad);
+			procent = double.Parse(zdrazeni);
+		}
+		catch
+		{
+			Console.WriteLine("Zadané hodnoty nejsou čísla!");
+			return;
+		}
+		Console.WriteLine("Zdražení pomocí funkce: {0}", ZdrazitFunkce(cena, procent / 100));
+		ZdrazitProcedura(ref cena, procent / 100);
+		Console.WriteLine("Zdražení pomocí procedury: {0}", cena);
+	}
+	private static double ZdrazitFunkce(double cena, double zdrazeni)
+	{
+		return (zdrazeni + 1) * cena;
+	}
+	private static void ZdrazitProcedura(ref double cena, double zdrazeni)
+	{
+		cena *= zdrazeni + 1;
 	}
 }
