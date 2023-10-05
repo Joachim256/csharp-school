f07cc43231445d14c5f231e44bfa94dcd8714432
joachim256 <jachymnet@seznam.cz>
2023-10-05 08:56:53 +0200

    Factorial

--- csharp-school/Program.cs
+++ csharp-school/Program.cs
@@ -1,5 +1,43 @@
 ﻿internal class Program{
 	private static void Main(string[] args){
-		
+		Console.WriteLine("Faktoriál kalkulačka v1.0");
+		Console.Write("! ");
+
+		uint a;
+		while (true)
+		{
+			try
+			{
+				a = uint.Parse(Console.ReadLine());
+				break;
+			}
+			catch
+			{
+				Console.WriteLine("To není číslo!");
+			}
+		}
+
+		Console.WriteLine("{0}! = {1} = {2}", a, factorial1(a), factorial2(a));
+	}
+
+	private static uint factorial1(uint n)
+	{
+		uint result = 1;
+		for(uint i = 2; i <= n; i++)
+		{
+			result *= i;
+		}
+		return result;
+	}
+	private static uint factorial2(uint n)
+	{
+		if(n == 0)
+		{
+			return 1;
+		}
+		else
+		{
+			return n * factorial2(n-1);
+		}
 	}
 }
