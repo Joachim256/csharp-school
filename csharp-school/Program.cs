49e91a2c5c879fd93f0c34be79dfd9e84a838066
joachim256 <jachymnet@seznam.cz>
2023-10-19 09:27:24 +0200

    CondenseFile function

--- csharp-school/Program.cs
+++ csharp-school/Program.cs
@@ -3,6 +3,25 @@ internal class Program
 {
 	private static void Main(string[] args)
 	{
-		string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @".\csharp-school\prvni.txt");
+		string prvni = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @".\csharp-school\prvni.txt");
+
+		CondenseFile(prvni);
+	}
+	private static void CondenseFile(string path)
+	{
+        string str = "";
+        using (var f = new StreamReader(path))
+		{
+			string line;
+			while ((line = f.ReadLine()) != null)
+			{
+				str += " " + line;
+			}
+			f.Close();
+		};
+		using (var f = new StreamWriter(path))
+		{
+			f.WriteLine(str);
+		}
 	}
 }
