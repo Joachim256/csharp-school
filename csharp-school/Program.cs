c5d6ca5ec9989d14c64f72275d1f579cccc624a4
joachim256 <jachymnet@seznam.cz>
2023-10-19 09:39:26 +0200

    SummarizeFile function

--- csharp-school/Program.cs
+++ csharp-school/Program.cs
@@ -4,8 +4,10 @@ internal class Program
 	private static void Main(string[] args)
 	{
 		string prvni = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @".\csharp-school\prvni.txt");
+		string pribeh = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @".\csharp-school\pribeh.txt");
 
-		CondenseFile(prvni);
+        CondenseFile(prvni);
+		SummarizeFile(pribeh);
 	}
 	private static void CondenseFile(string path)
 	{
@@ -22,6 +24,24 @@ internal class Program
 		using (var f = new StreamWriter(path))
 		{
 			f.WriteLine(str);
+		}
+	}
+	private static void SummarizeFile(string path)
+	{
+		var f = new StreamReader(path);
+		string text = f.ReadToEnd();
+		f.Close();
+
+		string[] words = text.Split(new char[] { ' ' });
+		int letters = 0;
+		foreach (var word in words)
+		{
+			letters += word.Length;
+		}
+
+		using (var f2 = new StreamWriter(path, true))
+		{
+			f2.WriteLine("\nSoubor obsahuje:\n" + words.Length.ToString() + " slov\n" + letters + " písmen");
 		}
 	}
 }
