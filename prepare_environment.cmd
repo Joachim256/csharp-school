3a8fc79810e464b1a1516a579c5dc851f299be7a
joachim256 <jachymnet@seznam.cz>
2023-11-02 09:28:57 +0100

    prepare environment script

---
+++ prepare_environment.cmd
@@ -0,0 +1,16 @@
+mkdir %appdata%\csharp-school
+cd %appdata%\csharp-school
+
+chcp 65001
+
+type NUL > prvni.txt
+echo Ahoj >> prvni.txt
+echo jak >> prvni.txt
+echo se >> prvni.txt
+echo máš. >> prvni.txt
+
+echo Paní Malinová se toho večera rozhodla psát a malovat. > pribeh.txt
+
+echo Paní Malinová se toho večera rozhodla psát a malovat. > serad.txt
+
+explorer .
