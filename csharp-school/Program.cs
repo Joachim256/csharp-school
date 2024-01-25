4a0c34ad5e3b0b7f7409541fa43645d1d913b3dd
joachim256 <jachymnet@seznam.cz>
2024-01-25 09:02:30 +0100

    Generate data

--- csharp-school/Program.cs
+++ csharp-school/Program.cs
@@ -1,5 +1,17 @@
 ﻿internal class Program{
 	private static void Main(string[] args){
-		
+		int[] arr = new int[100];
+		generateData(ref arr);
+        for (int i = 0; i < arr.Length; i++) { Console.WriteLine(arr[i]); }
+    }
+	private static void generateData(ref int[] arr)
+	{
+		var rng = new Random();
+		arr[0] = 0;
+		for(int i = 1; i < arr.Length; i++)
+		{
+			arr[i] = arr[i-1] + rng.Next(10);
+		}
 	}
+
 }
