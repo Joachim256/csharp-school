e8ad8dc390413c8ea4acfd7ebe56e424f0391394
joachim256 <jachymnet@seznam.cz>
2024-01-25 09:39:07 +0100

    Binary search

--- csharp-school/Program.cs
+++ csharp-school/Program.cs
@@ -3,6 +3,8 @@
 		int[] arr = new int[100];
 		generateData(ref arr);
         for (int i = 0; i < arr.Length; i++) { Console.WriteLine(arr[i]); }
+		int index = binarySearch(ref arr, 55);
+		Console.WriteLine($"Found on index: {index}");
     }
 	private static void generateData(ref int[] arr)
 	{
@@ -13,5 +15,27 @@
 			arr[i] = arr[i-1] + rng.Next(10);
 		}
 	}
+	private static int binarySearch(ref int[] arr, int sval)
+	{
+		arr[0] = sval;
+		int start = 1, end = arr.Length -1;
 
+		while (start < end)
+		{
+			int middle = (start + end) / 2;
+			if(arr[middle] == sval)
+			{
+				return middle;
+			}
+			else if (arr[middle] < sval)
+			{
+				start = middle + 1;
+			}else if (arr[middle] > sval)
+			{
+				end = middle - 1;
+			}
+		}
+		if (arr[start] == sval) { return start; }
+		return -1;
+	}
 }
