0831b544e4cdf170cf63e960d4ac26208a408d9b
joachim256 <jachymnet@seznam.cz>
2024-02-22 09:27:12 +0100

    Nice lines

--- WinFormsApp2/Form1.Designer.cs
+++ WinFormsApp2/Form1.Designer.cs
@@ -28,12 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            splitContainer1 = new SplitContainer();
+            nLines = new NumericUpDown();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)nLines).BeginInit();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.FixedPanel = FixedPanel.Panel2;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            splitContainer1.Orientation = Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Paint += splitContainer1_Panel1_Paint;
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(nLines);
+            splitContainer1.Panel2.Controls.Add(label1);
+            splitContainer1.Size = new Size(757, 478);
+            splitContainer1.SplitterDistance = 427;
+            splitContainer1.TabIndex = 0;
+            // 
+            // nLines
+            // 
+            nLines.Location = new Point(77, 7);
+            nLines.Name = "nLines";
+            nLines.Size = new Size(120, 23);
+            nLines.TabIndex = 2;
+            nLines.Value = new decimal(new int[] { 20, 0, 0, 0 });
+            nLines.ValueChanged += nLines_ValueChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Počet čar:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(757, 478);
+            Controls.Add(splitContainer1);
+            Name = "Form1";
+            Text = "Form1";
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)nLines).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private SplitContainer splitContainer1;
+        private Label label1;
+        private NumericUpDown nLines;
     }
 }
--- WinFormsApp2/Form1.cs
+++ WinFormsApp2/Form1.cs
@@ -6,5 +6,37 @@ namespace WinFormsApp2
         {
             InitializeComponent();
         }
+        private void render()
+        {
+            var grafika = splitContainer1.Panel1.CreateGraphics();
+            grafika.Clear(Color.White);
+            var pero = new Pen(Color.Red, 2);
+            Point origin = new Point(0, 0);
+
+            int nlines = (int)nLines.Value;
+            int width = splitContainer1.Panel1.Width;
+            int height = splitContainer1.Panel1.Height;
+
+            for (int i = 0; i < nlines - 1; i++)
+            {
+                int x = (i * width / (nlines - 1));
+                grafika.DrawLine(pero, origin, new Point(x, height));
+            }
+            for (int i = 0; i < nlines - 1; i++)
+            {
+                int y = (i * height / (nlines - 1));
+                grafika.DrawLine(pero, origin, new Point(width, y));
+            }
+            // corner line
+            grafika.DrawLine(pero, origin, new Point(width, height));
+        }
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            render();
+        }
+        private void nLines_ValueChanged(object sender, EventArgs e)
+        {
+            render();
+        }
     }
 }
