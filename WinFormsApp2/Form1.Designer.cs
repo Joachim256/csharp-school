d314ba5afa7eb675c3d1efa3e7b700909d893622
joachim256 <jachymnet@seznam.cz>
2024-03-07 08:40:56 +0100

    Prázdný sál

--- WinFormsApp2/Form1.Designer.cs
+++ WinFormsApp2/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinFormsApp2
 {
-    partial class Form1
+    partial class KinoForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            kinoPictureBox = new PictureBox();
+            loadBtn = new Button();
+            saveBtn = new Button();
+            filenameLabel = new Label();
+            ((System.ComponentModel.ISupportInitialize)kinoPictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // kinoPictureBox
+            // 
+            kinoPictureBox.Location = new Point(12, 12);
+            kinoPictureBox.Name = "kinoPictureBox";
+            kinoPictureBox.Size = new Size(776, 405);
+            kinoPictureBox.TabIndex = 0;
+            kinoPictureBox.TabStop = false;
+            kinoPictureBox.Paint += kinoPictureBox_Paint;
+            // 
+            // loadBtn
+            // 
+            loadBtn.Location = new Point(12, 423);
+            loadBtn.Name = "loadBtn";
+            loadBtn.Size = new Size(75, 23);
+            loadBtn.TabIndex = 1;
+            loadBtn.Text = "Načíst sál";
+            loadBtn.UseVisualStyleBackColor = true;
+            // 
+            // saveBtn
+            // 
+            saveBtn.Location = new Point(93, 423);
+            saveBtn.Name = "saveBtn";
+            saveBtn.Size = new Size(75, 23);
+            saveBtn.TabIndex = 2;
+            saveBtn.Text = "Uložit sál";
+            saveBtn.UseVisualStyleBackColor = true;
+            // 
+            // filenameLabel
+            // 
+            filenameLabel.AutoSize = true;
+            filenameLabel.Location = new Point(174, 427);
+            filenameLabel.Name = "filenameLabel";
+            filenameLabel.Size = new Size(0, 15);
+            filenameLabel.TabIndex = 3;
+            // 
+            // KinoForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 458);
+            Controls.Add(filenameLabel);
+            Controls.Add(saveBtn);
+            Controls.Add(loadBtn);
+            Controls.Add(kinoPictureBox);
+            Name = "KinoForm";
+            Text = "Evidence sedadel";
+            Load += KinoForm_Load;
+            ((System.ComponentModel.ISupportInitialize)kinoPictureBox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox kinoPictureBox;
+        private Button loadBtn;
+        private Button saveBtn;
+        private Label filenameLabel;
     }
 }
--- WinFormsApp2/Form1.cs
+++ WinFormsApp2/Form1.cs
@@ -1,10 +1,59 @@
 namespace WinFormsApp2
 {
-    public partial class Form1 : Form
+    public partial class KinoForm : Form
     {
-        public Form1()
+        Kinosal kino;
+        public KinoForm()
         {
             InitializeComponent();
+        }
+        class Kinosal
+        {
+            private int pocetRad = 15;
+            private int pocetSedadel = 30;
+            private bool[,] sedadla;
+            public Kinosal()
+            {
+                sedadla = new bool[pocetSedadel, pocetRad];
+            }
+            public Kinosal(int sirka, int vyska)
+            {
+                pocetRad = vyska;
+                pocetSedadel = sirka;
+                sedadla = new bool[sirka, vyska];
+            }
+            public void Render(Graphics grafica)
+            {
+                const int sedadloSizepx = 16;
+                const int mezeraSizepx = 2;
+                Brush brush;
+                for (int y = 0; y < pocetRad; y++)
+                {
+                    for (int x = 0; x < pocetSedadel; x++)
+                    {
+                        if (this.sedadla[x, y])
+                        {
+                            brush = Brushes.Green;
+                        }
+                        else
+                        {
+                            brush = Brushes.Red;
+                        }
+                        grafica.FillRectangle(brush, x * (sedadloSizepx + mezeraSizepx), y * (sedadloSizepx + mezeraSizepx), sedadloSizepx, sedadloSizepx);
+                    }
+                }
+            }
+        }
+
+        private void KinoForm_Load(object sender, EventArgs e)
+        {
+            kino = new Kinosal();
+            filenameLabel.Text = "Prázdný sál";
+        }
+
+        private void kinoPictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            kino.Render(e.Graphics);
         }
     }
 }
