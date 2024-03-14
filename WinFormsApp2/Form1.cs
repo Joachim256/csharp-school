9cb17e05e6a2d77d8b23b0ea2d8d634c40e1863d
joachim256 <jachymnet@seznam.cz>
2024-03-14 09:41:07 +0100

    Experiment

--- WinFormsApp2/Form1.cs
+++ WinFormsApp2/Form1.cs
@@ -6,5 +6,21 @@ namespace WinFormsApp2
         {
             InitializeComponent();
         }
+        Label[] napisy = new Label[10];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Label label = new Label();
+                label.Location = new Point(10, 10 + i*20);
+                label.Text = i.ToString();
+                label.Click += Say;
+                Controls.Add(label);
+            }
+        }
+        private void Say(object sender, EventArgs e)
+        {
+            // todo: reference the clicked component
+        }
     }
 }
